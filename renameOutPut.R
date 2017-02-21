9ab95a4aac867751a25eeb45610fd7b7ad5b44ae
iskallinn <iskallinn@gmail.com>
2017-02-21 13:05:32 +0100

    rename function vs 0.2

--- renameOutPut.R
+++ renameOutPut.R
@@ -1,15 +1,32 @@
 # Replace folders in adam.script
-RenameOutPut <- function ( root ,old_name ,new_name) { 
+# function is a bit cumbersome and should be written diffrently if it is run directly on the cluster
+# the idea is to change the output folder names, since you often copy/paste input files when designing 
+# folder structure
+RenameOutPut <- function ( root ) { 
   prmfiles <-
     list.files(path = root,
                pattern = "*.prm$",
                recursive = T)
   for ( i in 1:length(prmfiles)) {
-    path <- paste(root,prmfiles[i], sep = "/")
+    path <- paste(root,prmfiles[i], sep = "")
   prm.file <- readLines(path)
-    pos <- grep(pattern = paste("(OutDirectory='/usr/home/qgg/kari/)","(",old_name,")","(.*)", sep = ""), x=prm.file)
-    prm.file[pos ] <- gsub(pattern = paste("(OutDirectory='/usr/home/qgg/kari/)","(",old_name,")","(.*)", sep = ""),
-                           replacement = paste("\\1",new_name,"\\3", sep = ""), x=prm.file[pos])
+    pos <- grep(pattern = "(OutDirectory=)", x=prm.file)
+    prm.file[pos] <-
+      gsub(
+        pattern = paste("(OutDirectory=)", "(.*)", sep = ""),
+        replacement = paste(
+          "\\1",
+          "'/usr/home/qgg/kari/OP_ROUNDUP/GENOMIC/",
+          gsub(
+            x = prmfiles[i],
+            pattern = "(^.*/)(input.prm)",
+            replacement = "\\1"
+          ),
+          sep = ""
+        ),
+        "'",
+        x = prm.file[pos]
+      )
     writeLines(prm.file, path) }
     }
 
@@ -25,6 +42,6 @@ RenameOutPut <- function ( root ,old_name ,new_name) {
       prm.file[(pos-1) ] <- "/" 
       writeLines(prm.file, path) }
   }
-  root <- "C:/Users/au384062/Dropbox/Projects/ADAM/OP_ROUNDUP/"
+  root <- "C:/Users/au384062/Dropbox/Projects/ADAM/OP_ROUNDUP/GENOMIC/"
   RenameOutPut(root, old_name, new_name)
   
