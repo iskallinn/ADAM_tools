d1f72505e1a406f7736d885501fa65799ea3e78f
iskallinn <iskallinn@gmail.com>
2017-03-03 14:00:46 +0100

    commented and fixed a bit the super submit, so the script is ready to be launched from the terminal without any modification

--- SuperSubmit.R
+++ SuperSubmit.R
@@ -1,5 +1,9 @@
 # mass submit to quoue
-setwd( "C:/Users/au384062/Dropbox/Projects/ADAM/OP_ROUNDUP/MBLUP")
+# setwd( "C:/Users/au384062/Dropbox/Projects/ADAM/OP_ROUNDUP/MBLUP")
+
+# usage = change root folder to the folder which one wants to run the script
+# the script searches recursively through the subdirectories from root and makes a line with all script files
+
 root <- "C:/Users/au384062/Dropbox/Projects/ADAM/OP_ROUNDUP/MBLUP" 
 MakeSuperSubmit <- function ( root ) {
   scripts <-as.matrix(
@@ -9,5 +13,10 @@ MakeSuperSubmit <- function ( root ) {
   
 submit <- file(description =  "submit")
 con <- submit
-cat(paste("qsub ","/usr/home/qgg/kari/OP_ROUNDUP/MBLUP/", apply(scripts, 1, paste, "\n"), sep=""), file = con)
+cat(paste("#!/bin/bash"), file = con)
+cat("\n", file = con, append = T)
+###############################################################
+# NOTE the path below also needs to be changed!!!! ############
+###############################################################
+cat(paste("qsub ","/usr/home/qgg/kari/OP_ROUNDUP/MBLUP/", apply(scripts, 1, paste, "\n"), sep=""), file = con, append = T)
 }
