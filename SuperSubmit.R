8b295f36c6c5f4335b2bebcf199e0983390ae0b9
iskallinn <iskallinn@gmail.com>
2017-03-10 11:19:29 +0100

    Bugfix
    problem solved with not having input parameters in file. Still have to add #!/bin/bash and chmod 755 submit before submitting the submit

--- SuperSubmit.R
+++ SuperSubmit.R
@@ -1,11 +1,11 @@
 # mass submit to quoue
-setwd( "C:/Users/au384062/Dropbox/Projects/ADAM/OP_FORTITUDE/SBLUP")
+setwd( "C:/Users/au384062/Dropbox/Projects/adam_short/OP_FORTITUDE/GENOMIC/gs_opt/acc_high/SP/All_M_fNUC/")
 
 # usage = change root folder to the folder which one wants to run the script
 # the script searches recursively through the subdirectories from root and makes a line with all script files
 # best way would be to run this script on the cluster :) then it doesnt need two arguments
-root <- "C:/Users/au384062/Dropbox/Projects/ADAM/OP_FORTITUDE/SBLUP" 
-root.on.cluster <- "/usr/home/qgg/kari/OP_FORTITUDE/SBLUP/"
+root <- "C:/Users/au384062/Dropbox/Projects/adam_short/OP_FORTITUDE/GENOMIC/gs_opt/acc_high/SP/All_M_fNUC/" 
+root.on.cluster <- "/usr/home/qgg/kari/OP_FORTITUDE/GENOMIC/gs_opt/acc_high/SP/All_M_fNUC/"
 MakeSuperSubmit <- function ( root ,root.on.cluster) {
   # browser()
   scripts <-as.matrix(
@@ -20,7 +20,8 @@ con <- submit
 for (i in 1:length(scripts)) {
   cat(paste("cd", paste(root.on.cluster, paths[i], sep="")), "\n", file = con, append = T)
   # cat("\n", file = con, append = T)
-  cat(paste( "qsub ", paste(root.on.cluster,scripts[i]), sep=""),"\n", file = con, append =T)
+  cat(paste( "qsub adam_short.script \n"), file = con, append = T) 
+  # cat(paste( "qsub ", paste(root.on.cluster,scripts[i]), sep=""),"\n", file = con, append =T)
   # cat("\n", file = con, append = T)
 } 
-} 
+}  
